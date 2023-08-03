3302561dd39e72ab4431240332b2249977e3f052
giuliano <panzironi.giuliano@gmail.com>
2023-08-03 22:13:13 +0200

    Parametrize func by means of a YAML file

--- go.mod
+++ go.mod
@@ -2,7 +2,16 @@ module github.com/giulianopz/feedgen
 
 go 1.20
 
-require golang.org/x/net v0.13.0
+require (
+	github.com/go-faker/faker/v4 v4.1.1
+	golang.org/x/net v0.13.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
 
 require (
 	github.com/gorilla/feeds v1.1.1
--- main.go
+++ main.go
@@ -4,23 +4,102 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
 	"time"
 
+	"github.com/go-faker/faker/v4/pkg/slice"
 	"github.com/gorilla/feeds"
 	"golang.org/x/net/html"
+	"gopkg.in/yaml.v3"
 )
+
+type config struct {
+	baseUrl string
+
+	Title       string `yaml:"title,omitempty"`
+	Link        string `yaml:"link,omitempty"`
+	Description string `yaml:"description,omitempty"`
+	AuthorName  string `yaml:"author_name,omitempty"`
+	AuthorEmail string `yaml:"author_email,omitempty"`
+	FeedFormat  string `yaml:"feed_format,omitempty"`
+
+	ItemNode string   `yaml:"item_node,omitempty"`
+	ItemKey  string   `yaml:"item_key,omitempty"`
+	ItemVals []string `yaml:"item_vals,omitempty"`
+
+	HrefNode string `yaml:"href_node,omitempty"`
+
+	NoDate       bool   `yaml:"no_date,omitempty"`
+	SplittedDate bool   `yaml:"splitted_date,omitempty"`
+	DateFormat   string `yaml:"date_format,omitempty"`
+	FmtFormat    string `yaml:"fmt_format,omitempty"`
+
+	DateNode string `yaml:"date_node,omitempty"`
+	DateKey  string `yaml:"date_key,omitempty"`
+	DateVal  string `yaml:"date_val,omitempty"`
+
+	DayNode    string `yaml:"day_node,omitempty"`
+	DayKey     string `yaml:"day_key,omitempty"`
+	DayVal     string `yaml:"day_val,omitempty"`
+	DayDefault string `yaml:"day_default,omitempty"`
+
+	MonthNode    string `yaml:"month_node,omitempty"`
+	MonthKey     string `yaml:"month_key,omitempty"`
+	MonthVal     string `yaml:"month_val,omitempty"`
+	MonthDefault string `yaml:"month_default,omitempty"`
+
+	YearNode    string `yaml:"year_node,omitempty"`
+	YearKey     string `yaml:"year_key,omitempty"`
+	YearVal     string `yaml:"year_val,omitempty"`
+	YearDefault string `yaml:"year_default,omitempty"`
+}
+
+var usage string = "USAGE: feedgen YAML_FILE [OUT_DIR]"
 
 func main() {
 
+	if len(os.Args) < 2 {
+		fmt.Println(usage)
+	}
+
+	var (
+		configFile = os.Args[1]
+		outputDir  = "."
+	)
+
+	if len(os.Args) == 3 {
+		outputDir = os.Args[2]
+	}
+
+	bs, err := os.ReadFile(configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	config := new(config)
+	if err = yaml.Unmarshal(bs, config); err != nil {
+		panic(err)
+	}
+
+	url, err := url.Parse(config.Link)
+	if err != nil {
+		panic(err)
+	}
+
+	config.baseUrl = fmt.Sprintf("%s://%s", url.Scheme, url.Hostname())
+
 	feed := &feeds.Feed{
-		Title:       "The Bucklog: Essays and Rants",
-		Link:        &feeds.Link{Href: "https://weblog.jamisbuck.org/essays-and-rants/"},
-		Description: "Assorted ramblings by Jamis Buck",
-		Author:      &feeds.Author{Name: "Jamis Buck", Email: "jamis@jamisbuck.org"},
+		Title:       config.Title,
+		Link:        &feeds.Link{Href: config.Link},
+		Description: config.Description,
+		Author:      &feeds.Author{Name: config.AuthorName, Email: config.AuthorEmail},
 		Created:     time.Now(),
 	}
 
-	resp, err := http.Get("https://weblog.jamisbuck.org/essays-and-rants/")
+	resp, err := http.Get(config.Link)
 	if err != nil {
 		panic(err)
 	}
@@ -35,18 +114,25 @@ func main() {
 
 	var f func(*html.Node)
 	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "div" {
+		// div
+		if n.Type == html.ElementNode && n.Data == config.ItemNode {
 			for _, a := range n.Attr {
-				if a.Key == "class" && (a.Val == "post glimpse" || a.Val == "post link") {
+				// class
+				// "post glimpse", "post link"
+				if a.Key == config.ItemKey && slice.Contains(config.ItemVals, a.Val) {
 
-					item := findArticle(n)
+					item := NewFeedItem(n, config)
 
 					if !item.Created.IsZero() {
 						lastDate = item.Created
 					} else if !lastDate.IsZero() {
-						item.Created = lastDate.Add(24 * time.Hour)
+						item.Created = lastDate.Add(-24 * time.Hour)
 					} else {
-						lastDate = time.Date(1970, 1, 1, 1, 1, 1, 1, time.UTC)
+						parsed, err := time.Parse(config.DateFormat, fmt.Sprintf(config.FmtFormat, config.DayDefault, config.MonthDefault, config.YearDefault))
+						if err != nil {
+							panic(err)
+						}
+						lastDate = parsed
 					}
 
 					feed.Items = append(feed.Items, item)
@@ -60,44 +146,104 @@ func main() {
 	}
 	f(doc)
 
-	rss, err := feed.ToRss()
-	if err != nil {
-		log.Fatal(err)
+	var result string
+	switch config.FeedFormat {
+	case "rss":
+		{
+			rss, err := feed.ToRss()
+			if err != nil {
+				panic(err)
+			}
+			result = rss
+		}
+	case "atom":
+		{
+			atom, err := feed.ToAtom()
+			if err != nil {
+				panic(err)
+			}
+			result = atom
+		}
 	}
-	fmt.Println(rss)
+
+	if !strings.HasSuffix(outputDir, "/") {
+		outputDir += "/"
+	}
+
+	fileName := regexp.MustCompile("[^a-zA-Z]").ReplaceAllString(strings.ToLower(feed.Title), "-")
+	fileName = regexp.MustCompile(`\-+`).ReplaceAllString(fileName, "-")
+	if err := os.WriteFile(outputDir+fileName+".xml", []byte(result), os.ModePerm); err != nil {
+		panic(err)
+	}
 }
 
-func findArticle(node *html.Node) *feeds.Item {
+func NewFeedItem(node *html.Node, config *config) *feeds.Item {
 
 	item := &feeds.Item{
-		Author: &feeds.Author{Name: "Jamis Buck"},
+		Author: &feeds.Author{Name: config.AuthorName},
 	}
 
-	var month, year string
+	var day, month, year string
+	var date time.Time = time.Now()
 
 	var finder func(*html.Node)
 	finder = func(n *html.Node) {
 
-		if n.Type == html.ElementNode && n.Data == "a" {
+		// a
+		if n.Type == html.ElementNode && n.Data == config.HrefNode {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					//fmt.Println(a.Val)
-					item.Link = &feeds.Link{Href: "https://weblog.jamisbuck.org" + a.Val}
-					//fmt.Println(n.FirstChild.Data)
+
+					link := a.Val
+					if !strings.HasPrefix(link, "http") {
+						if !strings.HasPrefix(link, "/") {
+							link = "/" + link
+						}
+						link = config.baseUrl + link
+					}
+
+					item.Link = &feeds.Link{Href: link}
 					item.Title = n.FirstChild.Data
 				}
 			}
 		}
 
-		if n.Type == html.ElementNode && n.Data == "span" {
-			for _, a := range n.Attr {
-				if a.Key == "class" && a.Val == "month" {
-					//fmt.Println(n.FirstChild.Data)
-					month = n.FirstChild.Data
+		if !config.NoDate && config.SplittedDate {
+			if n.Type == html.ElementNode {
+
+				if n.Data == config.YearNode {
+					for _, a := range n.Attr {
+						if a.Key == config.YearKey && a.Val == config.YearVal {
+							year = n.FirstChild.Data
+						}
+					}
 				}
-				if a.Key == "class" && a.Val == "year" {
-					//fmt.Println(n.FirstChild.Data)
-					year = n.FirstChild.Data
+				if n.Data == config.MonthNode {
+					for _, a := range n.Attr {
+						if a.Key == config.MonthKey && a.Val == config.MonthVal {
+							month = n.FirstChild.Data
+						}
+					}
+				}
+				if n.Data == config.DayNode {
+					for _, a := range n.Attr {
+						if a.Key == config.DayKey && a.Val == config.DayVal {
+							day = n.FirstChild.Data
+						}
+					}
+				}
+
+			}
+		} else if !config.NoDate {
+			if n.Type == html.ElementNode && n.Data == config.DateNode {
+				for _, a := range n.Attr {
+					if a.Key == config.DateKey && a.Val == config.DateVal {
+						parsed, err := time.Parse(config.DateFormat, n.FirstChild.Data)
+						if err != nil {
+							panic(err)
+						}
+						date = parsed
+					}
 				}
 			}
 		}
@@ -109,12 +255,27 @@ func findArticle(node *html.Node) *feeds.Item {
 
 	finder(node)
 
-	date, err := time.Parse("02-Jan-2006", fmt.Sprintf("%s-%s-%s", "01", month, year))
-	if err != nil {
-		//fmt.Println(err)
-	} else {
-		item.Created = date
+	if !config.NoDate && config.SplittedDate {
+
+		d, m, y := day, month, year
+		if d == "" {
+			d = config.DayDefault
+		}
+		if m == "" {
+			m = config.MonthDefault
+		}
+		if y == "" {
+			y = config.YearDefault
+		}
+
+		parsed, err := time.Parse(config.DateFormat, fmt.Sprintf(config.FmtFormat, d, m, y))
+		if err != nil {
+			panic(err)
+		}
+		date = parsed
 	}
+
+	item.Created = date
 
 	return item
 }
